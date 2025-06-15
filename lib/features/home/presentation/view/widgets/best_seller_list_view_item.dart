2e216af6c32e60bfe5b10d77f1b9334a919d8669
Mohamed Soliman <mohamed.soliman@example.com>
2025-06-15 11:22:18 +0300

    best seller item done

--- lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/view/widgets/best_seller_list_view_item.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../constants.dart';
+import '../../../../../core/utils/styles.dart';
+import 'book_rating_item.dart';
+
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
@@ -8,23 +12,56 @@ class BestSellerListViewItem extends StatelessWidget {
     return Row(
       children: [
         SizedBox(
-          height:MediaQuery.of(context).size.height*0.18,
+          height: MediaQuery.of(context).size.height * 0.18,
           child: AspectRatio(
-            aspectRatio: 0.60 ,
+            aspectRatio: 0.60,
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.grey,
                 borderRadius: BorderRadius.circular(6),
                 image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage('assets/images/Book 1 High (1).png')),
-
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/Book 1 High (1).png'),
+                ),
               ),
-
             ),
           ),
         ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text('J.K. Rowling', style: Styles.textStyle14),
+                const SizedBox(height: 9),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                   const BookRatingItem(),
 
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
