8fd01fd470b4f2746fe742684579562322ac55f0
Mohamed Soliman <mohamed.soliman@example.com>
2025-06-24 13:58:36 +0300

    book cubit done

---
+++ lib/features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import '../../../data/model/Book_model.dart';
+import '../../../data/repos/home_repo.dart';
+
+part 'newest_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewsetBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books));
+      },
+    );
+  }
+}
